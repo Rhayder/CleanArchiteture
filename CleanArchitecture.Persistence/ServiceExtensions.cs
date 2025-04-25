6027a46e5a68ff7402b40c00cd79e4c92d07dd87
Heyder Barbosa <heydergalo@gmail.com>
2025-04-25 15:15:49 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchitecture.Persistence/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Sqlite");
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
